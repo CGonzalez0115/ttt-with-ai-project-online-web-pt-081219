ccb02861aa8935c7aaae20438f0b619df2d6c4a4
Christina Gonzalez <cmgamway@gmail.com>
2019-09-30 06:10:05 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,15 @@ class Board
 
   @cells = []
 
+  def initialize
+    reset!
+  end
+
   def reset!
     self.cells = Array.new(9, " ")
   end
 
-  def initialize
-    self.cells = Array.new(9, " ")
-  end
+
 
   def display
     puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
